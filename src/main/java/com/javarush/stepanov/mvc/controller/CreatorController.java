2c35121a8285eeefdc89f3fd51000eb5ec943e81
ruslstepanson753 <ruslstepanson753@gmail.com>
2025-04-10 15:48:03 +0300

    4 HATEOAS references

--- src/main/java/com/javarush/stepanov/mvc/controller/CreatorController.java
+++ src/main/java/com/javarush/stepanov/mvc/controller/CreatorController.java
@@ -2,119 +2,111 @@ package com.javarush.stepanov.mvc.controller;
 
 import com.javarush.stepanov.mvc.model.creator.Creator;
 import com.javarush.stepanov.mvc.service.CreatorService;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import org.springframework.data.domain.Page;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.NoSuchElementException;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
-@RequestMapping("api/v1.0/creators")
+@RequestMapping("/api/v2/creators")
 public class CreatorController {
 
     private final CreatorService creatorService;
+    private final PagedResourcesAssembler<Creator.Out> pagedResourcesAssembler;
 
-    public CreatorController(CreatorService creatorService) {
+    public CreatorController(CreatorService creatorService,
+                             PagedResourcesAssembler<Creator.Out> pagedResourcesAssembler) {
         this.creatorService = creatorService;
-    }
-
-    @GetMapping
-    public Collection<Creator.Out> getAll() {
-        return creatorService.getAll();
-    }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Creator.Out create(@RequestBody @Valid Creator.In inputDto) {
-        return creatorService.create(inputDto);
-    }
-
-    @PutMapping
-    @ResponseStatus(HttpStatus.OK)
-    public Creator.Out update(@RequestBody @Valid Creator.In inputDto) {
-        try {
-            return creatorService.update(inputDto);
-        } catch (NoSuchElementException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }
+        this.pagedResourcesAssembler = pagedResourcesAssembler;
     }
 
     @GetMapping("/{id}")
-    public Creator.Out read(@PathVariable long id) {
-        return creatorService.get(id);
+    public EntityModel<Creator.Out> getCreatorById(@PathVariable Long id) {
+        Creator.Out creator = creatorService.get(id);
+
+        return EntityModel.of(creator,
+                linkTo(methodOn(CreatorController.class).getCreatorById(id)).withSelfRel(),
+                linkTo(methodOn(CreatorController.class).getAllCreators(0, 10, "id::asc")).withRel("creators"),
+                linkTo(methodOn(CreatorController.class).getCreatorsByFirstname(creator.getFirstname(), 0, 10)).withRel("same-firstname"),
+                linkTo(methodOn(CreatorController.class).updateCreator(id, null)).withRel("update").withType("PUT"),
+                linkTo(methodOn(CreatorController.class).deleteCreator(id)).withRel("delete").withType("DELETE")
+        );
     }
 
+    @GetMapping
+    public ResponseEntity<PagedModel<EntityModel<Creator.Out>>> getAllCreators(
+            @Min(0) @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id::asc") String sortBy) {
 
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable long id) {
-        boolean delete = creatorService.delete(id);
-        if (!delete) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }
-    }
+        Page<Creator.Out> pageResult = creatorService.getAllPaged(page, size, sortBy);
 
-    @GetMapping("/paged")
-    public ResponseEntity<PagedModel<EntityModel<Creator.Out>>> getAllPaged(
-            @Min(1) @RequestParam(required = false, defaultValue = "1") int page,
-            @RequestParam(required = false, defaultValue = "10") int size,
-            @RequestParam(name = "sort_by", required = false, defaultValue = "id::asc") String sortBy,
-            PagedResourcesAssembler<Creator.Out> assembler) {
+        PagedModel<EntityModel<Creator.Out>> model = pagedResourcesAssembler.toModel(pageResult,
+                creator -> EntityModel.of(creator,
+                        linkTo(methodOn(CreatorController.class).getCreatorById(creator.getId())).withSelfRel(),
+                        linkTo(methodOn(CreatorController.class).getCreatorsByFirstname(creator.getFirstname(), 0, 10)).withRel("same-firstname")
+                ));
 
-        Page<Creator.Out> creatorPage = creatorService.getAllPaged(page - 1, size, sortBy);
+        // Добавляем дополнительные ссылки
+        model.add(linkTo(methodOn(CreatorController.class).createCreator(null)).withRel("create").withType("POST"));
 
-        PagedModel<EntityModel<Creator.Out>> pagedModel =
-                assembler.toModel(creatorPage, linkTo(methodOn(CreatorController.class).getAllPaged(page, size, sortBy, assembler)).withSelfRel());
-
-        HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.add("Link", createLinkHeader(pagedModel));
-
-        return new ResponseEntity<>(pagedModel, responseHeaders, HttpStatus.OK);
-    }
-
-    private String createLinkHeader(PagedModel<EntityModel<Creator.Out>> pagedModel) {
-        final StringBuilder linkHeader = new StringBuilder();
-
-        if (pagedModel.getLinks().hasLink("first")) {
-            linkHeader.append(buildLinkHeader(pagedModel.getLink("first").get().getHref(), "first"));
-            linkHeader.append(", ");
-        }
-
-        if (pagedModel.getLinks().hasLink("next")) {
-            linkHeader.append(buildLinkHeader(pagedModel.getLink("next").get().getHref(), "next"));
-        }
-
-        return linkHeader.toString();
-    }
-
-    public static String buildLinkHeader(final String uri, final String rel) {
-        return "<" + uri + ">; rel=\"" + rel + "\"";
+        return ResponseEntity.ok(model);
     }
 
     @GetMapping("/firstnames/{firstname}")
-    public Collection<Creator.Out> getCreatorsByFirstname(@PathVariable (value = "firstname", required = false) String firstname)
-    {
-        // Simulate retrieving product from a specific category
-        if (firstname == null) {
-            return creatorService.getAll();
-        }
+    public ResponseEntity<PagedModel<EntityModel<Creator.Out>>> getCreatorsByFirstname(
+            @PathVariable String firstname,
+            @Min(0) @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
 
-        Collection<Creator.Out> result = creatorService.getCreatorsByFirstname(firstname);
-        System.out.println();
-        return result;
+        // Здесь используем ту же логику, что и в getAllPaged, но с фильтрацией по имени
+        Page<Creator.Out> pageResult = (Page<Creator.Out>) creatorService.getAllPaged(page, size, "firstname::asc")
+                .filter(creator -> creator.getFirstname().equals(firstname));
+
+        PagedModel<EntityModel<Creator.Out>> model = pagedResourcesAssembler.toModel(pageResult,
+                creator -> EntityModel.of(creator,
+                        linkTo(methodOn(CreatorController.class).getCreatorById(creator.getId())).withSelfRel()
+                ));
+
+        model.add(linkTo(methodOn(CreatorController.class).getAllCreators(0, 10, "id::asc")).withRel("all-creators"));
+
+        return ResponseEntity.ok(model);
     }
 
+    @PostMapping
+    public ResponseEntity<EntityModel<Creator.Out>> createCreator(@RequestBody Creator.In input) {
+        Creator.Out created = creatorService.create(input);
+
+        return ResponseEntity
+                .created(linkTo(methodOn(CreatorController.class).getCreatorById(created.getId())).toUri())
+                .body(EntityModel.of(created,
+                        linkTo(methodOn(CreatorController.class).getCreatorById(created.getId())).withSelfRel(),
+                        linkTo(methodOn(CreatorController.class).getAllCreators(0, 10, "id::asc")).withRel("creators")
+                ));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EntityModel<Creator.Out>> updateCreator(
+            @PathVariable Long id,
+            @RequestBody Creator.In input) {
+        input.setId(id);
+        Creator.Out updated = creatorService.update(input);
+
+        return ResponseEntity.ok(EntityModel.of(updated,
+                linkTo(methodOn(CreatorController.class).getCreatorById(id)).withSelfRel(),
+                linkTo(methodOn(CreatorController.class).getAllCreators(0, 10, "id::asc")).withRel("creators")
+        ));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteCreator(@PathVariable Long id) {
+        creatorService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
